fca1ec3de316fa57ef2c4f358fbd349313d76878
keer2345 <keer2345@gmail.com>
2024-03-22 00:28:35 +0800

    pom.xml

---
+++ keer-shop-admin/src/main/java/WebApplication.java
@@ -0,0 +1,14 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+/**
+ * @author keer
+ * @date 2024-03-22
+ */
+@SpringBootApplication
+public class WebApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(WebApplication.class, args);
+  }
+}
