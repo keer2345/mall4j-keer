2376ce765ff29640edb0b90b78d3cfdaf8dcc5c9
keer2345 <keer2345@gmail.com>
2024-03-23 21:20:50 +0800

    keer-shop-admin

--- keer-shop-admin/src/main/java/com/keer/shop/admin/WebApplication.java
+++ keer-shop-admin/src/main/java/com/keer/shop/admin/WebApplication.java
@@ -1,6 +1,7 @@
+package com.keer.shop.admin;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author keer
