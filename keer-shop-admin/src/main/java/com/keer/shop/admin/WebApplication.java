cb85c41bbdaad994514fac62bbea795b621cec1b
keer2345 <keer2345@gmail.com>
2024-03-24 16:24:20 +0800

    add SpringBootServletInitializer to module of keer-shop-admin

--- keer-shop-admin/src/main/java/com/keer/shop/admin/WebApplication.java
+++ keer-shop-admin/src/main/java/com/keer/shop/admin/WebApplication.java
@@ -2,14 +2,28 @@ package com.keer.shop.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author keer
  * @date 2024-03-22
  */
 @SpringBootApplication
-public class WebApplication {
+@ComponentScan("com.keer.shop")
+public class WebApplication extends SpringBootServletInitializer {
+
   public static void main(String[] args) {
     SpringApplication.run(WebApplication.class, args);
+  }
+
+  /**
+   * SpringBootServletInitializer让SpringBoot将应用程序以 WAR 包的形式部署在外部的SERVLET容器中
+   * （如Tomcat、Jetty、Undertow等)，并提供一种简单的配置外部SERVLET容器的方法。
+   */
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(WebApplication.class);
   }
 }
